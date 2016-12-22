c0ac6717f18150ea75061cffdb3b151cef369f68
johnthedesigner <john@johnthedesigner.com>
2016-12-22 14:33:50 -0500

    Remove all boot script content

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,34 +1,34 @@
 module.exports = function(app) {
-  var Reviewer = app.models.reviewer;
-  var Role = app.models.Role;
-  var RoleMapping = app.models.RoleMapping;
-  var Team = app.models.Team;
-
-  Reviewer.findOrCreate({
-    where: {
-      username: 'JTD'
-    }
-  },
-  [
-      {
-        username: 'JTD',
-        email: 'john@johnthedesigner.com',
-        password: 'password'
-      }
-  ], function(err, reviewer) {
-    if (err) throw err;
-    // Create the admin role
-    Role.create({
-      name: 'admin'
-    }, function(err, role) {
-      if (err) throw err;
-      // Make JTD an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: reviewer.id
-      }, function(err, principal) {
-        if (err) throw err;
-      });
-    });
-  });
+  // var Reviewer = app.models.reviewer;
+  // var Role = app.models.Role;
+  // var RoleMapping = app.models.RoleMapping;
+  // var Team = app.models.Team;
+  //
+  // Reviewer.findOrCreate({
+  //   where: {
+  //     username: 'JTD'
+  //   }
+  // },
+  // [
+  //     {
+  //       username: 'JTD',
+  //       email: 'john@johnthedesigner.com',
+  //       password: 'password'
+  //     }
+  // ], function(err, reviewer) {
+  //   if (err) throw err;
+  //   // Create the admin role
+  //   Role.create({
+  //     name: 'admin'
+  //   }, function(err, role) {
+  //     if (err) throw err;
+  //     // Make JTD an admin
+  //     role.principals.create({
+  //       principalType: RoleMapping.USER,
+  //       principalId: reviewer.id
+  //     }, function(err, principal) {
+  //       if (err) throw err;
+  //     });
+  //   });
+  // });
 };
